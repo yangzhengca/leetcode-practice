5dc37d2b0f78d0badba57f69bb77a05e9c2aa8e2
yangzhengca <yangzhengca>
2022-02-15 12:49:49 -0600

    finished 704

--- 704-Binary-Search.js
+++ 704-Binary-Search.js
@@ -24,17 +24,31 @@
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
 
-
-
 // Solution 1:
 
 // var search = function(nums, target) {
 //     for (let i=0; i<nums.length; i++){
 //         if (nums[i] === target) {
 //             return i;
-//         } 
+//         }
 //     }
 //     return -1;
 // };
 
-
+// solution 2:
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1,
+    pivot;
+  while (left <= right) {
+    pivot = left + Math.floor((right - left) / 2);
+    if (nums[pivot] === target) {
+      return pivot;
+    } else if (nums[pivot] > target) {
+      right = pivot - 1;
+    } else {
+      left = pivot + 1;
+    }
+  }
+  return -1;
+};
